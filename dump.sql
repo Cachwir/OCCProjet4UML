3eff690b22c1f74be1f62030f3e60225e55eedb3
cachwir <cachwir@gmx.com>
2017-05-28 13:49:06 +0200

    Schema : + price data

--- dump.sql
+++ dump.sql
@@ -69,7 +69,8 @@ CREATE TABLE `dishes` (
   `name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `type` int(11) DEFAULT NULL,
   `description` text COLLATE utf8_unicode_ci,
-  `photos` longtext COLLATE utf8_unicode_ci
+  `photos` longtext COLLATE utf8_unicode_ci,
+  `price_ht` decimal(6,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -114,7 +115,10 @@ CREATE TABLE `orders_dishes` (
   `dish_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
   `dishes_id` int(11) NOT NULL,
-  `orders_id` int(11) NOT NULL
+  `orders_id` int(11) NOT NULL,
+  `price_ht` decimal(6,2) NOT NULL,
+  `price_ttc` decimal(6,2) NOT NULL,
+  `tva` decimal(6,2) COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
@@ -216,10 +220,14 @@ ALTER TABLE `orders_dishes`
 
 INSERT INTO `managers` (`id`, `firstname`, `lastname`, `phone`, `login`, `password`) VALUES (NULL, 'Serge ', 'D\'Assaut', '0645127820', 's.dassaut', '7d837c398f061197825ef5655e85b229'), (NULL, 'Christiane', 'Taubira', '07824519630', 'c.taubira', 'ef710546d1a89771dcecbef0f504ec81');
 
-INSERT INTO `dishes` (`id`, `date`, `name`, `type`, `description`, `photos`) VALUES (NULL, '2017-05-22', 'Velouté de lentilles', 'Délicat velouté de lentilles à la crème de fenouil.
-Les lentilles sont réputées idéales contre la fatigue et le fenouil fait un excellent antioxydant.', 1, '[http://www.francine.com/images/content/recettes/EN/crme-de-lentilles.jpg]'), (NULL, '2017-05-22', 'Estouffade d\'agneau aux cèpes', 'Servie avec sa farandole de céréales anciennes, cette estouffade d\'agneau et ses cèpes vous feront passer un repas d\'exception.', 1, '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fviande.2Fcanard.2Festouffade-de-cepes-a-la-gasconne-276734.2F2945227-1-fre-FR.2Festouffade-de-cepes-a-la-gasconne.2Ejpg/300x225/crop-from/center/estouffade-de-cepes-a-la-gasconne.jpeg]');
+INSERT INTO `dishes` (`id`, `date`, `name`, `type`, `description`, `photos`, `price_ht`) VALUES
+  (NULL, '2017-05-22', 'Velouté de lentilles', 'Délicat velouté de lentilles à la crème de fenouil.
+Les lentilles sont réputées idéales contre la fatigue et le fenouil fait un excellent antioxydant.', 1, '[http://www.francine.com/images/content/recettes/EN/crme-de-lentilles.jpg]', 7.20),
+  (NULL, '2017-05-22', 'Estouffade d\'agneau aux cèpes', 'Servie avec sa farandole de céréales anciennes, cette estouffade d\'agneau et ses cèpes vous feront passer un repas d\'exception.', 1, '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fviande.2Fcanard.2Festouffade-de-cepes-a-la-gasconne-276734.2F2945227-1-fre-FR.2Festouffade-de-cepes-a-la-gasconne.2Ejpg/300x225/crop-from/center/estouffade-de-cepes-a-la-gasconne.jpeg]', 14.50);
 
-INSERT INTO `dishes` (`id`, `date`, `name`, `type`, `description`, `photos`) VALUES (NULL, '2017-05-22', 'Tiramisu', '2', 'Infimement relevé au rhum, ce tiramisu ravira vos papilles. Chaque bouchée fondante vous donnera la sensation de déguster un nuage.', '[http://img-3.journaldesfemmes.com/45Ut-TllYOrlbrBiHuXtFEEHLkg=/750x/smart/image-icu/10021897_1450775950.jpg]'), (NULL, '2017-05-22', 'Tartelette au citron meringuée', '2', 'Un classique, mais pourtant si bon.', '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fdessert.2Ftartes.2Ftartelette-au-citron-meringuee-276907.2F3086262-1-fre-FR.2Ftartelette-au-citron-meringuee.2Ejpg/345x258/crop-from/center/tartelette-au-citron-meringuee.jpeg]');
+INSERT INTO `dishes` (`id`, `date`, `name`, `type`, `description`, `photos`, `price_ht`) VALUES
+  (NULL, '2017-05-22', 'Tiramisu', '2', 'Infimement relevé au rhum, ce tiramisu ravira vos papilles. Chaque bouchée fondante vous donnera la sensation de déguster un nuage.', '[http://img-3.journaldesfemmes.com/45Ut-TllYOrlbrBiHuXtFEEHLkg=/750x/smart/image-icu/10021897_1450775950.jpg]', 5.50),
+  (NULL, '2017-05-22', 'Tartelette au citron meringuée', '2', 'Un classique, mais pourtant si bon.', '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fdessert.2Ftartes.2Ftartelette-au-citron-meringuee-276907.2F3086262-1-fre-FR.2Ftartelette-au-citron-meringuee.2Ejpg/345x258/crop-from/center/tartelette-au-citron-meringuee.jpeg]', 4.50);
 
 INSERT INTO `deliverers` (`id`, `firstname`, `lastname`, `phone`, `login`, `password`, `status`, `gps_latitude`, `gps_longitude`) VALUES (NULL, 'Frédéric', 'Bouleau', '0782496530', 'f.bouleau', '40f69d39709c075dec09f2cb3c7542f0', '1', '48.862725', '2.287592'), (NULL, 'Amélie', 'Soubille', '0654217893', 'a.soubille', '40f69d39709c075dec09f2cb3c7542f0', '1', '48.864851', '2.376766');
 
@@ -227,4 +235,4 @@ INSERT INTO `customers` (`id`, `firstname`, `lastname`, `address`, `zip_code`, `
 
 INSERT INTO `orders` (`id`, `date`, `customer_id`, `deliverer_id`) VALUES (NULL, '1495472144', '1', '2');
 
-INSERT INTO `orders_dishes` (`id`, `quantity`, `dish_id`, `order_id`) VALUES (NULL, '1', '1', '1'), (NULL, '1', '4', '1');
+INSERT INTO `orders_dishes` (`id`, `quantity`, `dish_id`, `order_id`, `price_ht`, `price_ttc`, `tva`) VALUES (NULL, '1', '1', '1', 7.20, 9.36, 30), (NULL, '1', '4', '1', 4.50, 5.85, 30);
