26cd56419ee16c35809cb616d9f35e4f28b768b9
cachwir <cachwir@gmx.com>
2017-05-31 19:57:44 +0200

    Schema : + a couple of fields. Added the UML diagrams.

--- dump.sql
+++ dump.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.4
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2
+-- http://www.phpmyadmin.net
 --
--- Client :  127.0.0.1
--- Généré le :  Sam 20 Mai 2017 à 15:03
--- Version du serveur :  5.7.14
--- Version de PHP :  5.6.25
+-- Client :  localhost
+-- Généré le :  Mer 31 Mai 2017 à 19:48
+-- Version du serveur :  5.7.18-0ubuntu0.16.04.1
+-- Version de PHP :  7.0.18-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -36,8 +36,19 @@ CREATE TABLE `customers` (
   `gps_latitude` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `gps_longitude` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `comment` text COLLATE utf8_unicode_ci DEFAULT NULL
+  `comment` text COLLATE utf8_unicode_ci,
+  `login` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `payment_details` longtext COLLATE utf8_unicode_ci
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `customers`
+--
+
+INSERT INTO `customers` (`id`, `firstname`, `lastname`, `address`, `zip_code`, `city`, `gps_latitude`, `gps_longitude`, `phone`, `comment`, `login`, `password`, `email`, `payment_details`) VALUES
+  (1, 'Ginette', 'Pitras', '68 Avenue Denfert-Rochereau', 75014, 'Paris', '48.838509', '2.335625', '0154879236', '', 'warlord666', '7d837c398f061197825ef5655e85b229', 'g.pitras@gmail.com', '{}');
 
 -- --------------------------------------------------------
 
@@ -52,10 +63,19 @@ CREATE TABLE `deliverers` (
   `phone` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `login` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `status` int(11) DEFAULT NULL,
   `gps_latitude` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
   `gps_longitude` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `deliverers`
+--
+
+INSERT INTO `deliverers` (`id`, `firstname`, `lastname`, `phone`, `login`, `password`, `email`, `status`, `gps_latitude`, `gps_longitude`) VALUES
+  (1, 'Frédéric', 'Bouleau', '0782496530', 'f.bouleau', '40f69d39709c075dec09f2cb3c7542f0', 'f.bouleau@gmail.com', 1, '48.862725', '2.287592'),
+  (2, 'Amélie', 'Soubille', '0654217893', 'a.soubille', '40f69d39709c075dec09f2cb3c7542f0', 'a.soubille@gmail.com', 1, '48.864851', '2.376766');
 
 -- --------------------------------------------------------
 
@@ -70,8 +90,18 @@ CREATE TABLE `dishes` (
   `type` int(11) DEFAULT NULL,
   `description` text COLLATE utf8_unicode_ci,
   `photos` longtext COLLATE utf8_unicode_ci,
-  `price_ht` decimal(6,2) NOT NULL
+  `price_ht` decimal(6,2) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `dishes`
+--
+
+INSERT INTO `dishes` (`id`, `date`, `name`, `type`, `description`, `photos`, `price_ht`) VALUES
+  (1, '2017-05-22', 'Velouté de lentilles', NULL, 'Délicat velouté de lentilles à la crème de fenouil.\r\nLes lentilles sont réputées idéales contre la fatigue et le fenouil fait un excellent antioxydant.', '[http://www.francine.com/images/content/recettes/EN/crme-de-lentilles.jpg]', 7.5),
+  (2, '2017-05-22', 'Estouffade d\'agneau aux cèpes', NULL, 'Servie avec sa farandole de céréales anciennes, cette estouffade d\'agneau et ses cèpes vous feront passer un repas d\'exception.', '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fviande.2Fcanard.2Festouffade-de-cepes-a-la-gasconne-276734.2F2945227-1-fre-FR.2Festouffade-de-cepes-a-la-gasconne.2Ejpg/300x225/crop-from/center/estouffade-de-cepes-a-la-gasconne.jpeg]', 13),
+  (3, '2017-05-22', 'Tiramisu', 2, 'Infimement relevé au rhum, ce tiramisu ravira vos papilles. Chaque bouchée fondante vous donnera la sensation de déguster un nuage.', '[http://img-3.journaldesfemmes.com/45Ut-TllYOrlbrBiHuXtFEEHLkg=/750x/smart/image-icu/10021897_1450775950.jpg]', 5.5),
+  (4, '2017-05-22', 'Tartelette au citron meringuée', 2, 'Un classique, mais pourtant si bon.', '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fdessert.2Ftartes.2Ftartelette-au-citron-meringuee-276907.2F3086262-1-fre-FR.2Ftartelette-au-citron-meringuee.2Ejpg/345x258/crop-from/center/tartelette-au-citron-meringuee.jpeg]', 4.5);
 
 -- --------------------------------------------------------
 
@@ -88,6 +118,14 @@ CREATE TABLE `managers` (
   `password` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Contenu de la table `managers`
+--
+
+INSERT INTO `managers` (`id`, `firstname`, `lastname`, `phone`, `login`, `password`) VALUES
+  (1, 'Serge ', 'D\'Assaut', '0645127820', 's.dassaut', '7d837c398f061197825ef5655e85b229'),
+  (2, 'Christiane', 'Taubira', '07824519630', 'c.taubira', 'ef710546d1a89771dcecbef0f504ec81');
+
 -- --------------------------------------------------------
 
 --
@@ -96,12 +134,17 @@ CREATE TABLE `managers` (
 
 CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `customer_id` int(11) DEFAULT NULL,
-  `deliverer_id` int(11) DEFAULT NULL,
   `date` int(11) DEFAULT NULL,
-  `customers_id` int(11) NOT NULL,
-  `deliverers_id` int(11) NOT NULL
+  `customer_id` int(11) NOT NULL,
+  `deliverer_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Contenu de la table `orders`
+--
+
+INSERT INTO `orders` (`id`, `date`, `customer_id`, `deliverer_id`) VALUES
+  (1, 1495472144, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -111,15 +154,21 @@ CREATE TABLE `orders` (
 
 CREATE TABLE `orders_dishes` (
   `id` int(11) NOT NULL,
-  `order_id` int(11) NOT NULL,
-  `dish_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL,
-  `dishes_id` int(11) NOT NULL,
-  `orders_id` int(11) NOT NULL,
+  `dish_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
   `price_ht` decimal(6,2) NOT NULL,
   `price_ttc` decimal(6,2) NOT NULL,
-  `tva` decimal(6,2) COLLATE utf8_unicode_ci
+  `tva` decimal(6,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `orders_dishes`
+--
+
+INSERT INTO `orders_dishes` (`id`, `quantity`, `dish_id`, `order_id`, `price_ht`, `price_ttc`, `tva`) VALUES
+  (1, 1, 1, 1, '0.00', '0.00', '0.00'),
+  (2, 1, 4, 1, '0.00', '0.00', '0.00');
 
 --
 -- Index pour les tables exportées
@@ -154,16 +203,16 @@ ALTER TABLE `managers`
 --
 ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_orders_customers1_idx` (`customers_id`),
-  ADD KEY `fk_orders_deliverers1_idx` (`deliverers_id`);
+  ADD KEY `fk_orders_customers1_idx` (`customer_id`),
+  ADD KEY `fk_orders_deliverers1_idx` (`deliverer_id`);
 
 --
 -- Index pour la table `orders_dishes`
 --
 ALTER TABLE `orders_dishes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_orders_dishes_dishes_idx` (`dishes_id`),
-  ADD KEY `fk_orders_dishes_orders1_idx` (`orders_id`);
+  ADD KEY `fk_orders_dishes_dishes_idx` (`dish_id`),
+  ADD KEY `fk_orders_dishes_orders1_idx` (`order_id`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
@@ -173,17 +222,17 @@ ALTER TABLE `orders_dishes`
 -- AUTO_INCREMENT pour la table `customers`
 --
 ALTER TABLE `customers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `deliverers`
 --
 ALTER TABLE `deliverers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT pour la table `dishes`
 --
 ALTER TABLE `dishes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT pour la table `managers`
 --
@@ -193,12 +242,12 @@ ALTER TABLE `managers`
 -- AUTO_INCREMENT pour la table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT pour la table `orders_dishes`
 --
 ALTER TABLE `orders_dishes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Contraintes pour les tables exportées
 --
@@ -207,32 +256,16 @@ ALTER TABLE `orders_dishes`
 -- Contraintes pour la table `orders`
 --
 ALTER TABLE `orders`
-  ADD CONSTRAINT `fk_orders_customers1` FOREIGN KEY (`customers_id`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_orders_deliverers1` FOREIGN KEY (`deliverers_id`) REFERENCES `deliverers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_orders_customers1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_orders_deliverers1` FOREIGN KEY (`deliverer_id`) REFERENCES `deliverers` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Contraintes pour la table `orders_dishes`
 --
 ALTER TABLE `orders_dishes`
-  ADD CONSTRAINT `fk_orders_dishes_dishes` FOREIGN KEY (`dishes_id`) REFERENCES `dishes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_orders_dishes_orders1` FOREIGN KEY (`orders_id`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_orders_dishes_dishes` FOREIGN KEY (`dish_id`) REFERENCES `dishes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_orders_dishes_orders1` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
-
-INSERT INTO `managers` (`id`, `firstname`, `lastname`, `phone`, `login`, `password`) VALUES (NULL, 'Serge ', 'D\'Assaut', '0645127820', 's.dassaut', '7d837c398f061197825ef5655e85b229'), (NULL, 'Christiane', 'Taubira', '07824519630', 'c.taubira', 'ef710546d1a89771dcecbef0f504ec81');
-
-INSERT INTO `dishes` (`id`, `date`, `name`, `type`, `description`, `photos`, `price_ht`) VALUES
-  (NULL, '2017-05-22', 'Velouté de lentilles', 'Délicat velouté de lentilles à la crème de fenouil.
-Les lentilles sont réputées idéales contre la fatigue et le fenouil fait un excellent antioxydant.', 1, '[http://www.francine.com/images/content/recettes/EN/crme-de-lentilles.jpg]', 7.20),
-  (NULL, '2017-05-22', 'Estouffade d\'agneau aux cèpes', 'Servie avec sa farandole de céréales anciennes, cette estouffade d\'agneau et ses cèpes vous feront passer un repas d\'exception.', 1, '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fviande.2Fcanard.2Festouffade-de-cepes-a-la-gasconne-276734.2F2945227-1-fre-FR.2Festouffade-de-cepes-a-la-gasconne.2Ejpg/300x225/crop-from/center/estouffade-de-cepes-a-la-gasconne.jpeg]', 14.50);
-
-INSERT INTO `dishes` (`id`, `date`, `name`, `type`, `description`, `photos`, `price_ht`) VALUES
-  (NULL, '2017-05-22', 'Tiramisu', '2', 'Infimement relevé au rhum, ce tiramisu ravira vos papilles. Chaque bouchée fondante vous donnera la sensation de déguster un nuage.', '[http://img-3.journaldesfemmes.com/45Ut-TllYOrlbrBiHuXtFEEHLkg=/750x/smart/image-icu/10021897_1450775950.jpg]', 5.50),
-  (NULL, '2017-05-22', 'Tartelette au citron meringuée', '2', 'Un classique, mais pourtant si bon.', '[http://img.cac.pmdstatic.net/fit/http.3A.2F.2Fwww.2Ecuisineactuelle.2Efr.2Fvar.2Fcui.2Fstorage.2Fimages.2Frecettes-de-cuisine.2Fdessert.2Ftartes.2Ftartelette-au-citron-meringuee-276907.2F3086262-1-fre-FR.2Ftartelette-au-citron-meringuee.2Ejpg/345x258/crop-from/center/tartelette-au-citron-meringuee.jpeg]', 4.50);
-
-INSERT INTO `deliverers` (`id`, `firstname`, `lastname`, `phone`, `login`, `password`, `status`, `gps_latitude`, `gps_longitude`) VALUES (NULL, 'Frédéric', 'Bouleau', '0782496530', 'f.bouleau', '40f69d39709c075dec09f2cb3c7542f0', '1', '48.862725', '2.287592'), (NULL, 'Amélie', 'Soubille', '0654217893', 'a.soubille', '40f69d39709c075dec09f2cb3c7542f0', '1', '48.864851', '2.376766');
-
-INSERT INTO `customers` (`id`, `firstname`, `lastname`, `address`, `zip_code`, `city`, `gps_latitude`, `gps_longitude`, `phone`, `comment`) VALUES (NULL, 'Ginette', 'Pitras', '68 Avenue Denfert-Rochereau', '75014', 'Paris', '48.838509', '2.335625', '0154879236', 'Sonner à l\'interphone, au numéro 3.');
-
-INSERT INTO `orders` (`id`, `date`, `customer_id`, `deliverer_id`) VALUES (NULL, '1495472144', '1', '2');
-
-INSERT INTO `orders_dishes` (`id`, `quantity`, `dish_id`, `order_id`, `price_ht`, `price_ttc`, `tva`) VALUES (NULL, '1', '1', '1', 7.20, 9.36, 30), (NULL, '1', '4', '1', 4.50, 5.85, 30);
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
